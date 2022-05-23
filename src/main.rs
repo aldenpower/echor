2c64e5ea1ab4d112b9adc8a0b297b686543eb75a
aldenpower <anderson_afsl@hotmail.com>
2022-05-23 05:27:51 -0300

    add dependencies and test

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use std::process::Command;
-use std::env::args;
 use clap::{App, Arg};
 
 fn main() {
-    let matches = App::new("echo")
+    let matches = App::new("echor")
         .version("0.1.0")
         .author("aldenpower <eng.andersonfsl@gmail.com>")
         .about("rust echo")
@@ -22,9 +20,16 @@ fn main() {
         )
         .get_matches();
 
+    let text = matches.values_of_lossy("text").unwrap();
+    let omit_newline= matches.is_present("omit_newline");
 
 
-    println!("{:#?}", matches);
+    print!("{}{}", text.join(" "), if omit_newline { "" } else { "\n" });
+
+
+
+
+    // println!("{:#?}", matches);
 
 }
 
